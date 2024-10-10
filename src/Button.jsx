71d463aed6613b598a8f404aef0370a9a4dfc595
Udaya Krishna <118593303+Udaya-Krishna@users.noreply.github.com>
2024-10-10 12:56:29 +0530

    Delete src/Button.jsx

--- src/Button.jsx
+++
@@ -1,8 +0,0 @@
-
-function Button(){
-
-    const handleClick = (e) => e.target.textContent = "ouch!";
-    return(<button onDoubleClick={(e) => handleClick(e)}>Click me</button>);
-}
-
-export default Button
